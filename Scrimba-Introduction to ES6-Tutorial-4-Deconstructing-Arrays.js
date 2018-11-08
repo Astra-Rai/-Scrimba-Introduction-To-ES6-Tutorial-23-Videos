27875f2639fffc769226fab635e9b2b94d0bce3b
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-08 16:24:35 -0600

    Deconstructing Arrays

---
+++ Scrimba-Introduction to ES6-Tutorial-4-Deconstructing-Arrays.js
@@ -0,0 +1,12 @@
+//Decontructing Arrays
+
+//[firstName] is storying a reference to index 0, that stores 
+let [firstName, middleName, lastName] = ['Astra', 'the one and only', 'Rai'];
+//console.log(firstName);
+//console.log(middleName);
+//consoel.log(lastName);
+
+//the contents of firstName can be changed
+
+firstName = 'Star';
+console.log(firstName);

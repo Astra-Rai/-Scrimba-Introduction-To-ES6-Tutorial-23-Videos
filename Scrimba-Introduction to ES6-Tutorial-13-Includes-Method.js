ef6ab527d790231f5f472fd5f5f8b9277769066f
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-09 18:01:29 -0600

    Includes Method

---
+++ Scrimba-Introduction to ES6-Tutorial-13-Includes-Method.js
@@ -0,0 +1,12 @@
+//Includes Method
+
+/*The includes() method determines whether an array includes a certain element, returning true or false as appropriate. */
+
+
+let numArray = [1,2,3,4,5];
+//check if an array had a value, we'd use indexOf, now we use includes
+console.log(numArray.includes(0));//false
+console.log(numArray.includes(2));//true
+
+
+

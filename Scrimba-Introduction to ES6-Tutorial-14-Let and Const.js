9673d738505bf983d38213706371da3c2ebc27a9
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-10 20:32:59 -0600

    Let && Const

---
+++ Scrimba-Introduction to ES6-Tutorial-14-Let and Const.js
@@ -0,0 +1,20 @@
+//example 1
+
+/*fi(false) {
+    var example = 5;
+  //if let is used instead of var, returns undefined
+}
+console.log(example);//null
+//let is a stricter version of var, uses block scope
+//var using hoisting
+//let, const no hoisting
+*/
+
+//primitiave data type, number, string, boolean
+const example =5;
+//example = 10//const, when dealing with primitative types, are read-only
+example = 10;
+console.log(example);
+
+//when dealing with arrays, we can push values
+//you can modify objects and arrays, but you cannot override the datatypes.

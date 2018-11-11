7db77a39ad04f8e39cb4be7aff6e773333bc6519
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-11-10 21:49:33 -0600

    padStart and padEnd Methods Challenge

---
+++ Scrimba-Introduction to ES6-Tutorial-17-padStart and padEnd  Methods -Challenge.js
@@ -0,0 +1,8 @@
+
+
+let example = 'YouTube.com/CodingTutorials360';
+
+// console.log(example.padStart(100));//prints an empty string, length is still 100
+// console.log(example.padEnd(1));//ingnore b/c string is longer than one empty character
+
+//note to self: review both methods and other examples
